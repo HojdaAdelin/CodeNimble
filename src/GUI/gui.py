fb80c3ca8bf6f50f4cadf098f0434d6c2c9dc940
HojdaAdelin <adelinionut297@gmail.com>
2024-02-14 19:57:55 +0200

    Main menu

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,11 +1,50 @@
 import customtkinter as ct
+from CTkMenuBar import *
 from ctypes import byref, sizeof, c_int, windll
 
 class MainWindow(ct.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Main menu
+        menu = CTkMenuBar(self, bg_color="#333333")
+        home = menu.add_cascade("Home")
+        file = menu.add_cascade("File")
+        edit = menu.add_cascade("Edit")
+        view = menu.add_cascade("View")
+
+        home_drop = CustomDropdownMenu(widget=home, padx=-5, pady=-25, bg_color="#333333", font=("", 14), corner_radius=4)
+        home_drop.add_option(option="Version")
+        home_drop.add_option(option="Change log")
+        home_drop.add_option(option="Source")
+        home_drop.add_option(option="Exit")
+
+        file_drop = CustomDropdownMenu(widget=file, padx=-55, pady=-25, bg_color="#333333", font=("", 14), corner_radius=4)
+        file_drop.add_option(option="New window")
+        file_drop.add_option(option="New")
+        file_drop.add_option(option="Open")
+        file_drop.add_option(option="Save")
+        file_drop.add_option(option="Save as")
+
+        edit_drop = CustomDropdownMenu(widget=edit, padx=-95, pady=-25, bg_color="#333333", font=("", 14), corner_radius=4)
+        edit_drop.add_option(option="Undo")
+        edit_drop.add_option(option="Redo")
+        edit_drop.add_option(option="Cut")
+        edit_drop.add_option(option="Copy")
+        edit_drop.add_option(option="Paste")
+        edit_drop.add_option(option="Delete")
+        edit_drop.add_option(option="Select all")
+        edit_drop.add_option(option="Find")
+        edit_drop.add_option(option="Replace")
+
+        view_drop = CustomDropdownMenu(widget=view, padx=-135, pady=-25, bg_color="#333333", font=("", 14), corner_radius=4)
+        view_drop.add_option(option="Zoom in")
+        view_drop.add_option(option="Zoom out")
+        # General configuration
+        ct.set_default_color_theme("dark-blue")
         self.title("CodeNimble")
+        self.geometry("1200x700")
+        # Title bar color handle
         HWND = windll.user32.GetParent(self.winfo_id())
         tb_color = 0x333333
         windll.dwmapi.DwmSetWindowAttribute(
@@ -16,5 +55,4 @@ class MainWindow(ct.CTk):
 
 if __name__ == "__main__":
     window = MainWindow()
-    window.geometry("1200x700")
     window.mainloop()
--- src/main.py
+++ src/main.py
@@ -2,5 +2,4 @@ from GUI.gui import MainWindow
 
 if __name__ == "__main__":
     window = MainWindow()
-    window.geometry("300x200")
     window.mainloop()
