8b3f96a23a020603520fbd95554672726842572b
HojdaAdelin <adelinionut297@gmail.com>
2024-02-17 21:02:25 +0200

    Line count

--- src/GUI/textbox.py
+++ src/GUI/textbox.py
@@ -1,7 +1,38 @@
 import customtkinter as ct
+import tkinter as tk
 
 class TextBox(ct.CTkTextbox):
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack(fill="both", expand=True)
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.pack(side="right", fill="both", expand=True)
         self.configure(fg_color="#222222", wrap='none', font=("Helvetica", 20))
+        self.linenumbers = TextLineNumbers(master, width=110)
+        self.linenumbers.configure(bg="#222222")
+        self.linenumbers.attach(self)
+        self.linenumbers.pack(side="left", fill="y")
+
+    def update_line_numbers(self, *args):
+        self.linenumbers.redraw()
+
+class TextLineNumbers(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        tk.Canvas.__init__(self, *args, **kwargs, highlightthickness=0)
+        self.textwidget = None
+        self.font = kwargs.get("font", ("Helvetica", 28))  # Fontul implicit
+
+    def attach(self, text_widget):
+        self.textwidget = text_widget
+
+    def redraw(self, *args):
+        self.delete("all")
+        i = self.textwidget.index("@0,0")
+        while True:
+            dline = self.textwidget.dlineinfo(i)
+            if dline is None:
+                break
+            y = dline[1]
+            linenum = str(i).split('.')[0]
+            x = self.winfo_width() - 2  # Calculați coordonata x folosind lățimea canvas-ului
+            self.create_text(x, y, anchor="ne", text=linenum, fill="#888888", font=self.font)  # Specificați fontul aici
+            i = self.textwidget.index("%s+1line" % i)
+    
--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import customtkinter as ct
 
 if __name__ == "__main__":
     window = MainWindow()
-    # Main frame 
     main_frame = ct.CTkFrame(window)
     main_frame.pack(fill="both", expand=True)
     text = TextBox(main_frame)
+    text.bind("<KeyRelease>", text.update_line_numbers)
     window.mainloop()
