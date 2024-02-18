d767c236e50c3bfe566a63407ef39f8730a5506f
HojdaAdelin <adelinionut297@gmail.com>
2024-02-18 15:27:37 +0200

    Line1

--- src/GUI/textbox.py
+++ src/GUI/textbox.py
@@ -6,29 +6,33 @@ class TextBox(ct.CTkTextbox):
         super().__init__(master, *args, **kwargs)
         self.pack(side="right", fill="both", expand=True)
         self.configure(fg_color="#222222", wrap='none', font=("Helvetica", 18))
-        self.linenumbers = TextLineNumbers(master, width=5, height=self.cget("height"))
-        self.linenumbers.configure(background="#333333", foreground="white", font=("Helvetica", 28))
+        self.linenumbers = TextLineNumbers(master, width=50, height=self.cget("height"))
+        self.linenumbers.configure(fg_color="#333333", font=("Helvetica", 18))
         self.linenumbers.attach(self)
         self.linenumbers.pack(side="left", fill="y")
     def update_line_numbers(self, *args):
         self.linenumbers.redraw()
 
-class TextLineNumbers(tk.Text):
+class TextLineNumbers(ct.CTkTextbox):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.textwidget = None
-        self.config(state="disabled", pady=10)  # Adăugăm un padding în partea de sus
+        self.configure(state="disabled")  # Configurăm textul ca fiind dezactivat pentru a fi doar pentru vizualizare
 
     def attach(self, text_widget):
         self.textwidget = text_widget
 
     def redraw(self, *args):
-        self.config(state="normal")  # Activăm textul pentru a putea fi modificat
+        self.configure(state="normal")  # Activăm textul pentru a putea fi modificat
         self.delete("1.0", "end")
+
+        # Obținem dimensiunea maximă a unui număr de linie
+        max_linenum_width = len(str(self.textwidget.index("end").split(".")[0]))
+
         i = self.textwidget.index("@0,0")
         line_count = int(self.textwidget.index("end-1c").split('.')[0])
         for linenum in range(1, line_count + 1):
-            self.insert("end", str(linenum) + "\n")
-        self.config(state="disabled")  # Redăm textul inactiv pentru a-l face doar pentru vizualizare
-        self.tag_add("right", "1.0", "end")  # Adăugăm un tag pentru a alinia textul la dreapta
-        self.tag_config("right", justify="right")  # Configurăm tagul pentru aliniere la dreapta
+            self.insert("end", str(linenum).rjust(max_linenum_width) + "\n")  # Aliniem la dreapta numărul de linie
+            self.tag_add("right", f"{linenum}.0", f"{linenum}.end")  # Adăugăm tagul pentru aliniere la dreapta
+            self.tag_config("right", justify="right")  # Configurăm tagul pentru aliniere la dreapta
+        self.configure(state="disabled")  # Redăm textul inactiv pentru a-l face doar pentru vizualizare
