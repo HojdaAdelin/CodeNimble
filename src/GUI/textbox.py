dfd9b0e09ab95ceba22c3b3f6ccf4469c886c08f
HojdaAdelin <adelinionut297@gmail.com>
2024-02-19 20:26:49 +0200

    Line3

--- src/GUI/textbox.py
+++ src/GUI/textbox.py
@@ -4,15 +4,23 @@ import tkinter as tk
 class TextBox(ct.CTkTextbox):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
+        self.vertical_scrollbar = ct.CTkScrollbar(master, command=self.multi_scroll)
+        self.vertical_scrollbar.pack(side="right", fill="y")
         self.pack(side="right", fill="both", expand=True)
         self.configure(fg_color="#222222", wrap='none', font=("Helvetica", 18))
         self.linenumbers = TextLineNumbers(master, width=50, height=self.cget("height"))
-        self.linenumbers.configure(fg_color="#333333", font=("Helvetica", 18))
+        self.linenumbers.configure(fg_color="#333333", font=("Helvetica", 18), corner_radius=0, pady=5)
         self.linenumbers.attach(self)
+        self.linenumbers._scrollbars_activated = False
+        self._scrollbars_activated = False
         self.linenumbers.pack(side="left", fill="y")
+        self.configure(yscrollcommand=self.vertical_scrollbar.set)
+        self.linenumbers.configure(yscrollcommand=self.vertical_scrollbar.set)
     def update_line_numbers(self, *args):
         self.linenumbers.redraw()
-
+    def multi_scroll(self, *args):
+        self.yview(*args)
+        self.linenumbers.yview(*args)
 class TextLineNumbers(ct.CTkTextbox):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
