914c99698762c213617176b27efcc04ea6ec5092
HojdaAdelin <adelinionut297@gmail.com>
2024-04-03 20:31:46 +0300

    Save & save as file

--- src/MainMenu/file_menu.py
+++ src/MainMenu/file_menu.py
@@ -1,25 +1,26 @@
 import tkinter as tk
-import customtkinter as ctk
 from tkinter import filedialog
 
-import sys
-import os
-
-current_dir = os.path.dirname(__file__)
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-sys.path.append(parent_dir)
-
-from GUI import gui
+# Definim o variabilă globală pentru a ține numele fișierului deschis
+opened_filename = None
 
 def new_file(text, window):
+    global opened_filename  # Specificăm că vrem să folosim variabila globală
+
+    # Resetează numele fișierului deschis anterior
+    opened_filename = None
+
     text.delete("1.0", tk.END)
     window.redraw()
 
 def open_file(text, window):
+    global opened_filename  # Specificăm că vrem să folosim variabila globală
+
     filename = filedialog.askopenfilename(filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
 
     if filename:
-        
+        opened_filename = filename  # Actualizăm numele fișierului deschis
+
         with open(filename, "r") as file:
             file_content = file.read()
 
@@ -27,9 +28,27 @@ def open_file(text, window):
             text.insert("1.0", file_content) 
             window.redraw()
 
+def save_file(text):
+    global opened_filename  # Specificăm că vrem să folosim variabila globală
 
-def save_file():
-    pass
+    if opened_filename:
+        content = text.get("1.0", tk.END)
 
-def save_as_file():
-    pass
+        with open(opened_filename, "w") as file:
+            file.write(content)
+    else:
+        # Dacă nu există un fișier deschis anterior, apelăm funcția save_as_file()
+        save_as_file(text)
+
+def save_as_file(text):
+    global opened_filename  # Specificăm că vrem să folosim variabila globală
+
+    content = text.get("1.0", tk.END)
+    filename = filedialog.asksaveasfilename(defaultextension=".txt",
+                                             filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+
+    if filename:
+        opened_filename = filename  # Actualizăm numele fișierului deschis
+
+        with open(filename, "w") as file:
+            file.write(content)
