718e42cb6d20f52e2fe9f51a6ff9d02b5d5bf79b
HojdaAdelin <adelinionut297@gmail.com>
2024-04-07 22:10:09 +0300

    Fix zoom

--- src/MainMenu/view_menu.py
+++ src/MainMenu/view_menu.py
@@ -11,10 +11,13 @@ from Config import check
 
 def zoom_in(root):
     ante_size = check.get_config_value("zoom")
-    check.update_config_file("zoom", int(ante_size) + 4)
+    if (int(ante_size) + 4 < 50):
+        check.update_config_file("zoom", int(ante_size) + 4)
     root.redraw()
 
 def zoom_out(root):
     ante_size = check.get_config_value("zoom")
-    check.update_config_file("zoom", int(ante_size) - 4)
+    if (int(ante_size) - 4 > 20):
+        check.update_config_file("zoom", int(ante_size) - 4)
+    
     root.redraw()
