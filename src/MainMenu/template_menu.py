bbc76da6100c1f14a8881e9abaf59b02fc269f8a
HojdaAdelin <adelinionut297@gmail.com>
2024-05-03 18:15:53 +0300

    New templates

--- src/MainMenu/template_menu.py
+++ src/MainMenu/template_menu.py
@@ -10,6 +10,59 @@ sys.path.append(parent_dir)
 from MainMenu import file_menu
 from MainMenu import edit_menu
 
+cpp_text = """#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World";
+    return 0;
+}"""
+
+c_text = """#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}"""
+
+java_text = """public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}"""
+
+html_text = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>"""
+
+competitive_text = """#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solution() {
+    
+}
+
+int main()
+{
+    solution();
+
+    return 0;
+}"""
+
 def cpp_template(textbox, root, statusbar):
     # Textul șablonului
     template_text = """#include <iostream>
