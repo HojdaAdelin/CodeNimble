a31f3f0b52aeb3c73668f028ba33b3c69f9e2fa9
HojdaAdelin <adelinionut297@gmail.com>
2024-05-17 21:47:06 +0300

    Finish run code

--- src/MainMenu/run.py
+++ src/MainMenu/run.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
-
 import sys
+import tkinter as tk
+from tkinter import messagebox
 
 current_dir = os.path.dirname(__file__)
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
@@ -11,20 +12,28 @@ from MainMenu import file_menu
 
 def run_cpp_file():
     file_path = file_menu.current_file()
+    if file_path is None:
+        messagebox.showerror("Error", "No files are open.")
+        return
+
     if not file_path.endswith(".cpp"):
-        print("Doar fișierele .cpp pot fi rulate.")
+        messagebox.showerror("Error", "Only .cpp files can be run.")
         return
     
     # Compilarea fișierului .cpp
     executable_name = file_path.replace(".cpp", ".exe")
-    compile_command = f"g++ {file_path} -o {executable_name}"
+    error_log = file_path.replace(".cpp", "_error.log")
+    compile_command = f"g++ {file_path} -o {executable_name} 2> {error_log}"
     
     # Rularea comenzii de compilare
     compile_process = subprocess.run(compile_command, shell=True)
     
     if compile_process.returncode != 0:
-        return
-    
-    # Rularea fișierului executabil în cmd
-    run_command = f"start cmd /k {executable_name}"
-    subprocess.run(run_command, shell=True)
+        # Dacă există erori de compilare, deschidem cmd și afișăm erorile
+        run_command = f"start cmd /k type {error_log}"
+        subprocess.run(run_command, shell=True)
+    else:
+        # Dacă compilarea reușește, rulăm fișierul executabil în cmd
+        run_command = f"start cmd /k {executable_name}"
+        subprocess.run(run_command, shell=True)
+
