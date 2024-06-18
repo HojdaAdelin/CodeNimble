d0aa3d11d17a87f7296e2f5da8fb29a2cfcc1f0e
HojdaAdelin <adelinionut297@gmail.com>
2024-06-18 17:57:34 +0300

    Client names

--- src/Server/client.py
+++ src/Server/client.py
@@ -1,17 +1,19 @@
 import socket
 
 class Client:
-    def __init__(self, host='127.0.0.1', port=9999):
+    def __init__(self, host='127.0.0.1', port=9999, client_name=""):
         self.host = host
         self.port = port
+        self.client_name = client_name
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect_to_server()
+
+    def connect_to_server(self):
         self.client_socket.connect((self.host, self.port))
+        self.client_socket.send(self.client_name.encode())  # Trimite numele clientului la server
 
     def send_message(self, message):
-        try:
-            self.client_socket.send(message.encode())
-        except Exception as e:
-            print(f"Error sending message: {e}")
+        self.client_socket.send(message.encode())
 
     def receive_messages(self, callback):
         while True:
@@ -19,7 +21,7 @@ class Client:
                 message = self.client_socket.recv(1024).decode()
                 if message:
                     callback(message)
-            except Exception as e:
-                print(f"Error receiving message: {e}")
+            except:
+                print("An error occurred!")
                 self.client_socket.close()
                 break
