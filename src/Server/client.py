92a1c47e357a3b8d46edb75ba0d289fbfeef50b3
Hojda Adelin <adelinionut297@gmail.com>
2024-07-10 16:55:55 +0300

    LAN password system

--- src/Server/client.py
+++ src/Server/client.py
@@ -1,16 +1,33 @@
 import socket
 
 class Client:
-    def __init__(self, host='127.0.0.1', port=9999, client_name=""):
+    def __init__(self, host='127.0.0.1', port=9999, client_name="", password="1234"):
         self.host = host
         self.port = port
         self.client_name = client_name
+        self.password = password
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect_to_server()
 
     def connect_to_server(self):
         self.client_socket.connect((self.host, self.port))
-        self.client_socket.send(self.client_name.encode())  # Trimite numele clientului la server
+        self.client_socket.send(self.client_name.encode())  # Send client name
+        
+        # Wait for password prompt
+        password_response = self.client_socket.recv(1024).decode()
+        if password_response == "PASSWORD_REQUIRED":
+            self.client_socket.send(self.password.encode())
+        else:
+            print("Server rejected connection: Wrong password.")
+            self.client_socket.close()
+            return
+        
+        # Receive password acceptance
+        password_acceptance = self.client_socket.recv(1024).decode()
+        if password_acceptance != "PASSWORD_ACCEPTED":
+            print("Server rejected connection: Wrong password.")
+            self.client_socket.close()
+            return
 
     def send_message(self, message):
         self.client_socket.send(message.encode())
